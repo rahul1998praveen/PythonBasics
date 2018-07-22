8e079ea5a4806ad40333b271441fbedda8afefb1
rahul praveen <rahulpraveen16@gmail.com>
2018-07-22 18:26:41 +0530

    added further more charecteristics(almost completed the project)

--- basicProfitStatements/profitStatements.py
+++ basicProfitStatements/profitStatements.py
@@ -9,25 +9,15 @@ expenses = [12051.82, 5695.07, 12319.20, 12089.72,
             8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96]
 
 counter = 0
-r = np.array(revenue)
-e = np.array(expenses)
-
-
 profitEachMonth = []
-profitEachMonth = r-e
+while counter < 12:
+    profitEachMonth.append(revenue[counter] - expenses[counter])
+    counter = counter + 1
 
 
 months = ["January", 'February', 'March', 'April', 'May', 'June', 'July',
           'August', 'September', 'October', 'November', 'December']
-
-meanProfitForYear = statistics.mean(profitEachMonth)
-
-profitAfterTax = []
-i = 0
-while i < len(r):
-    profitAfterTax.append((profitEachMonth[i]-(profitEachMonth[i]*0.3)))
-
-
+meanProfitForYear = float(statistics.mean(profitEachMonth))
 # print(profitEachMonth)
 
 
@@ -37,7 +27,7 @@ def base_profit(month):
 
 def profit_after_tax(month):
     print("Profit after tax of 30% for the month", months[month], 'is',
-          (profitEachMonth[month]-(profitEachMonth[month]*0.3)))
+          (profitEachMonth[month]*0.7))
 
 
 def profit_margin(month):
@@ -45,4 +35,45 @@ def profit_margin(month):
           ((revenue[month]-expenses[month])/revenue[month])*100, '%')
 
 
+def worst_month():
+    worstMonthIndex = []
+    worstMonths = []
+    for i in range(0, 12):
+        if profitEachMonth[i] < meanProfitForYear:
+            worstMonthIndex.append(i)
+    print('The following were the bad months ('
+          'i.e Months which had profit less than the average mean profit for the year)')
+    for count in range(0, len(worstMonthIndex)):
+        print(months[worstMonthIndex[count]])
+        worstMonths.append(profitEachMonth[worstMonthIndex[count]])
+    print('The worst month of the year was : ', months[profitEachMonth.index(min(worstMonths))])
+
+
+def best_month():
+    bestMonthIndex = []
+    bestMonths = []
+    for i in range(0, 12):
+        if profitEachMonth[i] > meanProfitForYear:
+            bestMonthIndex.append(i)
+    print('The following were the good months '
+          '(i.e Months which had profit more than the average mean profit for the year)')
+    for count in range(0, len(bestMonthIndex)):
+        print(months[bestMonthIndex[count]])
+        bestMonths.append(profitEachMonth[bestMonthIndex[count]])
+    print('The best month of the year was : ', months[profitEachMonth.index(max(bestMonths))])
+
+
+# dictionary of functions for the program
+
+
 base_functions = {0: base_profit, 1: profit_after_tax, 2: profit_margin}
+monthNumber = 0
+while monthNumber < 12:
+    print('The Following is the information for the month', months[monthNumber], '\n')
+    for count_2 in range(0, 3):
+        base_functions[count_2](monthNumber)
+    monthNumber = monthNumber + 1
+    print('-----------------*---------------------*--------------------*------------------------')  # delimiter
+
+
+best_month()
